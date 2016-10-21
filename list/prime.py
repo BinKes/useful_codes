4c9fc2a72e167bded7f87ee244f239a8f82227b9
BinKes <289087901@qq.com>
2016-10-21 17:26:13 +0800

    'update prime'

--- list/prime.py
+++ list/prime.py
@@ -3,6 +3,10 @@
 #3到100内的素数
 prime1 = [x for x in range(3, 101, 2) 
     if not [y for y in range(3, int(x**0.5)+1, 2) if x%y ==0]]
-
-
 print(prime1)
+
+#判断一个数是不是素数
+def isPrime(n):
+    return True if n in [x for x in range(3, n+1, 2) if not [y for y in range(3, int(x**0.5)+1, 2) if x%y ==0]] else False
+    
+print(isPrime(20))
